52d502e82faa3de1182b1c1dc823ba2df8e2fc5f
elordeiro <estevao.lordeiro@hotmail.com>
2024-08-16 18:45:34 -0500

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+	// "strings"
 )
 
 func main() {
 
-	var input string
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		fmt.Scanln(&input)
 
-		switch input {
+		scanner.Scan()
+		line := strings.Split(strings.TrimSpace(scanner.Text()), " ")
+		cmd := line[0]
+
+		switch cmd {
+		case "exit":
+			code := atoi(line[1])
+			os.Exit(code)
 		default:
-			fmt.Printf("%s: command not found\n", input)
+			fmt.Printf("%s: command not found\n", cmd)
 		}
 	}
+}
+
+func atoi(a string) int {
+	i, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
 }
