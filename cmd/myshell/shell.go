2f1cfae1326db795259a880d7ea206bcb7215036
elordeiro <estevao.lordeiro@hotmail.com>
2024-08-17 02:38:57 -0500

    codecrafters submit [skip ci]

--- cmd/myshell/shell.go
+++ cmd/myshell/shell.go
@@ -12,17 +12,17 @@ import (
 )
 
 type Shell struct {
+	pwd      string
 	path     []string
 	builtins []string
 	scanner  *bufio.Scanner
-	pwd      string
 	// status   int
 }
 
 func NewShell() *Shell {
 	shell := &Shell{
 		path:     append([]string{}, strings.Split(os.Getenv("PATH"), ":")...),
-		builtins: append([]string{}, "echo", "exit", "pwd", "type"),
+		builtins: append([]string{}, "cd", "echo", "exit", "pwd", "type"),
 		scanner:  bufio.NewScanner(os.Stdin),
 		pwd:      os.Getenv("PWD"),
 	}
@@ -35,27 +35,25 @@ func (s *Shell) run() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		s.scanner.Scan()
-		line := s.scanner.Text()
-		splitLine := strings.Split(strings.TrimSpace(line), " ")
-		cmd := splitLine[0]
+		cmd, input := extCmd(s.scanner.Text())
 
 		switch cmd {
 		case "cd":
-			s.cd(line)
+			s.cd(input)
 		case "echo":
-			s.echo(line)
+			s.echo(input)
 		case "exit":
-			s.exit(line)
+			s.exit(input)
 		case "pwd":
 			s.pwdCmd()
 		case "type":
-			s.typeCmd(line)
+			s.typeCmd(input)
 		default:
 			if path, ok := s.cmdExists(cmd); ok {
-				s.call(path, splitLine[1:])
-			} else {
-				fmt.Printf("%s: command not found\n", cmd)
+				s.call(path, input)
+				continue
 			}
+			fmt.Printf("%s: command not found\n", cmd)
 		}
 	}
 }
@@ -63,65 +61,113 @@ func (s *Shell) run() {
 // ----------------------------------------------------------------------------
 // Commands
 // ----------------------------------------------------------------------------
-func (s *Shell) cd(line string) {
-	line = strings.TrimSpace(line)
-	if line == "cd" {
-		// implement cd home
-		// return
-	}
-
-	path := strings.TrimPrefix(line, "cd ")
-	stat, err := os.Stat(path)
-	if err != nil || !stat.IsDir() {
-		fmt.Printf("cd: %s: No such file or directory\n", path)
+func (s *Shell) cd(path string) {
+	if path == "" || path == "~" {
+		s.pwd = "~"
 		return
 	}
-	s.pwd = path
+
+	// absolute path
+	if path[0] == '/' || path[0] != '.' {
+		stat, err := os.Stat(path)
+		if err != nil || !stat.IsDir() {
+			fmt.Printf("cd: %s: No such file or directory\n", path)
+			return
+		}
+		s.pwd = path
+		return
+	}
+
+	// going up
+	for strings.HasPrefix(path, "..") {
+		// at root
+		if s.pwd == "/" {
+			return
+		}
+		s.pwd = s.pwd[:strings.LastIndex(s.pwd, "/")]
+		path = strings.TrimPrefix(path, "..")
+		path = strings.TrimPrefix(path, "/")
+	}
+
+	// staying here
+	if path == "" || path == "." {
+		return
+	}
+
+	// going down
+	path = strings.TrimPrefix(path, "./")
+
+	// get next level down
+	slash := strings.Index(path, "/")
+
+	// at bottom
+	if slash == -1 {
+		stat, err := os.Stat(s.pwd + "/" + path)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		if !stat.IsDir() {
+			fmt.Printf("cd: %s: Not a directory", path)
+			return
+		}
+		s.pwd += "/" + path
+		os.Setenv("PWD", s.pwd)
+		return
+	}
+
+	// recur
+	s.pwd += "/" + path[:slash]
+	s.cd("./" + path[slash+1:])
 }
 
-func (s *Shell) exit(line string) {
-	line = strings.TrimSpace(strings.TrimPrefix(line, "exit"))
-	if line == "" {
+func (s *Shell) exit(input string) {
+	if input == "" {
 		os.Exit(0)
 	}
-	code := atoi(line)
+	code := atoi(input)
 	os.Exit(code)
 }
 
-func (s *Shell) echo(line string) {
-	if line == "echo" {
+func (s *Shell) echo(str string) {
+	if str == "" {
 		fmt.Println()
 		return
 	}
-	line = strings.TrimPrefix(line, "echo ")
-	fmt.Println(line)
+	fmt.Println(str)
 }
 
 func (s *Shell) pwdCmd() {
 	fmt.Println(s.pwd)
 }
 
-func (s *Shell) typeCmd(line string) {
-	line = strings.TrimSpace(strings.TrimPrefix(line, "type"))
-	if line == "" {
+func (s *Shell) typeCmd(cmd string) {
+	if cmd == "" {
 		fmt.Println("type: missing argument")
 		return
 	}
 
-	if slices.Contains(s.builtins, line) {
-		fmt.Println(line, "is a shell builtin")
+	// command is a builtin cmd
+	if slices.Contains(s.builtins, cmd) {
+		fmt.Println(cmd, "is a shell builtin")
 		return
 	}
 
-	if path, ok := s.cmdExists(line); ok {
-		fmt.Println(line, "is", path)
+	// command is in path
+	if path, ok := s.cmdExists(cmd); ok {
+		fmt.Println(cmd, "is", path)
 		return
 	}
 
-	fmt.Printf("%s: not found\n", line)
+	// command does not exist
+	fmt.Printf("%s: not found\n", cmd)
 }
 
-func (s *Shell) call(path string, args []string) {
+func (s *Shell) call(path string, input string) {
+	var args []string
+	if input != "" {
+		args = strings.Split(input, " ")
+	}
 	cmd := exec.Command(path, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -151,7 +197,15 @@ func (s *Shell) cmdExists(cmd string) (path string, exists bool) {
 	return "", false
 }
 
-// func pathExists()
+func (s *Shell) setenv(key, value string) {
+	switch key {
+	case "pwd":
+		s.pwd = value
+	case "path":
+		s.path = append([]string{}, strings.Split(value, ":")...)
+	}
+	os.Setenv(key, value)
+}
 
 func atoi(a string) int {
 	i, err := strconv.Atoi(a)
@@ -159,6 +213,12 @@ func atoi(a string) int {
 		log.Fatal(err)
 	}
 	return i
+}
+
+func extCmd(input string) (cmd string, rest string) {
+	tokens := strings.Split(strings.TrimSpace(input), " ")
+	cmd = tokens[0]
+	return cmd, strings.Join(tokens[1:], " ")
 }
 
 // ----------------------------------------------------------------------------
